337c82d08379fd4eeca3760ad398fd3fbafe6dd2
qsaucy <quentin.saucy@heig-vd.ch>
2020-10-05 13:22:58 +0200

    refactor sql script

--- user.sql
+++ user.sql
@@ -1,13 +1,13 @@
 DROP if exists userSti
 CREATE TABLE userSti (
-    Id integer PRIMARY KEY AUTOINCREMENT,
+    id integer PRIMARY KEY AUTOINCREMENT,
     username varchar (50) UNIQUE NOT NULL,
     password varchar (255) NOT NULL,
     isAdmin INT(1) NOT NULL,
     isActive INT(1) NOT NULL
 );
 CREATE TABLE message(
-    Id integer PRIMARY KEY  AUTOINCREMENT,
+    id integer PRIMARY KEY  AUTOINCREMENT,
     receiptDate DATE NOT NULL,
     sender integer NULL,
     receiver integer NULL,
